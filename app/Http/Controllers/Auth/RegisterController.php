e3bf86f931f712cdbbd531d4fefd8581f3b79289
vagrant <vagrant@homestead>
2020-01-31 16:20:35 +0000

    nv

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,6 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'role' => ['required', 'int'],
         ]);
     }
 
