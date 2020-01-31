832c6755be64ac96267059719461a0242340f469
alex <Alejandro>
2020-01-31 17:11:28 +0100

    Primera fase: creando estructura

---
+++ database/migrations/2020_01_31_155128_create_listas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateListasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->int('idUsuarioCreador')->unique();
+            $table->string('titulo');
+            $table->string('descripcion');
+            $table->string('password');
+            $table->json('elementos');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listas');
+    }
+}
