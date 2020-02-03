1b2a29a79a5fdf21b216c15f9609d8d9c2b7fa4f
alex <Alejandro>
2020-02-03 17:39:58 +0100

    Seeders Finalizado

---
+++ database/seeds/ListasTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class ListasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('listas')->insert([
+            'idUsuarioCreador' => 1,
+            'titulo' => 'Lista Borja',
+            'descripcion'=> 'La lista de Borja',
+            'passwordLista' => Hash::make('1234'),
+            'elementos'=>str_replace("'", "\'", json_encode(['Comprar platanos','Comprar Melocotones'])),
+        ]);
+
+        DB::table('listas')->insert([
+            'idUsuarioCreador' => 2,
+            'titulo' => 'Lista Jose',
+            'descripcion'=> 'La lista de Jose',
+            'passwordLista' => Hash::make('1234'),
+            'elementos'=>str_replace("'", "\'", json_encode(['Comprar agua','Comprar lapices'])),
+        ]);
+    }
+}
