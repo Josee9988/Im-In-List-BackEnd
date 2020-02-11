cfc6c5d6773379863c023d4b7cb7055c6443dc73
alex <Alejandro>
2020-02-11 16:34:13 +0100

    usuario add in route

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', 'APIController@login');
-Route::post('register', 'api\usuariosController@store');
+Route::post('register', 'APIController@register');
 
 Route::group(['middleware' => 'auth.jwt'], function () {
 
