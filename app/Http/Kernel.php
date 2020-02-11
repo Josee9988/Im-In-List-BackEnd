0d9fb52979a0e3ac9e9935df89953aa8e66659ac
alex <Alejandro>
2020-02-11 15:34:52 +0100

    listas datos

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        
+        \Fruitcake\Cors\HandleCors::class
     ];
 
     /**
@@ -65,7 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'auth.jwt'  =>  \Tymon\JWTAuth\Http\Middleware\Authenticate::class, // JWT middleware
-        'cors' => \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,29 +1,25 @@
 <?php
 
-Route::group(['middleware' => 'cors'], function () {
+Route::post('login', 'APIController@login');
+Route::post('register', 'APIController@register');
 
-    Route::post('login', 'APIController@login');
-    Route::post('register', 'APIController@register');
+Route::group(['middleware' => 'auth.jwt'], function () {
 
-    Route::group(['middleware' => 'auth.jwt'], function () {
+    Route::get('logout', 'APIController@logout');
 
-        Route::get('logout', 'APIController@logout');
+    Route::get('user', 'APIController@getAuthenticatedUser');
 
-        Route::get('user', 'APIController@getAuthenticatedUser');
+    Route::get('listasAdmin', 'api\listasController@listasAdmin');
 
-        Route::get('listasAdmin', 'api\listasController@listasAdmin');
+    Route::get('listas', 'api\listasController@index')->name('getListas');
+    Route::get('listas/{id}', 'api\listasController@show')->name('getLista');
+    Route::post('listas', 'api\listasController@store')->name('addLista');
+    Route::put('listas/{id}', 'api\listasController@update')->name('editLista');
+    Route::delete('listas/{id}', 'api\listasController@destroy')->name('delLista');
 
-        Route::get('listas', 'api\listasController@index')->name('getListas');
-        Route::get('listas/{id}', 'api\listasController@show')->name('getLista');
-        Route::post('listas', 'api\listasController@store')->name('addLista');
-        Route::put('listas/{id}', 'api\listasController@update')->name('editLista');
-        Route::delete('listas/{id}', 'api\listasController@destroy')->name('delLista');
-
-        Route::get('users', 'api\usuariosController@index')->name('getUsers');
-        Route::get('users/{id}', 'api\usuariosController@show')->name('getUser');
-        Route::post('users', 'api\usuariosController@store')->name('addUser');
-        Route::put('users/{id}', 'api\usuariosController@update')->name('editUser');
-        Route::delete('users/{id}', 'api\usuariosController@destroy')->name('delUser');
-    });
-
+    Route::get('users', 'api\usuariosController@index')->name('getUsers');
+    Route::get('users/{id}', 'api\usuariosController@show')->name('getUser');
+    Route::post('users', 'api\usuariosController@store')->name('addUser');
+    Route::put('users/{id}', 'api\usuariosController@update')->name('editUser');
+    Route::delete('users/{id}', 'api\usuariosController@destroy')->name('delUser');
 });
